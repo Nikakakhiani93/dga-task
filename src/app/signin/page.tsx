902a082b5f96992f51d597dc52f067742affb98b
nikakakhiani93 <nikakakhiani93@gmail.com>
2023-11-11 22:18:56 +0400

    sign in UI, profile sidebar UI

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -32,12 +32,12 @@ export default function Page() {
   };
 
   return (
-    <div className='relative flex flex-col items-center justify-center min-h-screen overflow-hidden'>
-      <div className='w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl'>
+    <div className='bg-main-background relative main flex flex-col items-center justify-center min-h-screen overflow-hidden'>
+      <div className='w-full p-6 bg-white rounded-md lg:max-w-md shadow-2 pt-28 pb-16 rounded-[20px]'>
         <h1 className='text-3xl font-bold text-center text-gray-700'>
           Sign In
         </h1>
-        <p className='text-dimgray'>
+        <p className='flex justify-center text-[14px] text-[#6C6C6C] mt-3 pb-6'>
           {' '}
           Enter your credentials to access your account
         </p>
@@ -45,35 +45,37 @@ export default function Page() {
           <div className='mb-4'>
             <label
               htmlFor='email'
-              className='block text-sm font-semibold text-gray-800'
+              className='block text-sm font-medium text-[#6C6C6C]'
             >
               Email
             </label>
             <input
               id='email'
+              placeholder='Ender your email'
               onChange={(e) => setEmail(e.target.value)}
               type='email'
-              className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40'
+              className='placeholder: text-[12px] block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40'
             />
           </div>
-          <div className='mb-2'>
+          <div className='mb-2 mt-6'>
             <label
               htmlFor='password'
-              className='block text-sm font-semibold text-gray-800'
+              className='block text-sm font-medium text-[#6C6C6C]'
             >
               Password
             </label>
             <input
               id='password'
+              placeholder='Ender your password'
               onChange={(e) => setPassword(e.target.value)}
               type='password'
-              className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40'
+              className='placeholder: text-[12px] block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40'
             />
           </div>
 
           <div className='mt-2'>
             <button
-              className='w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600'
+              className='w-full px-4 py-2 tracking-wide text-white text-[14px] mt-6 transition-colors duration-200 transform bg-[#FEAF00] rounded-md hover:bg-gray-600 focus:outline-none '
               onClick={(e) => {
                 e.preventDefault();
                 auth();
@@ -88,7 +90,7 @@ export default function Page() {
           Forgot your password ?{' '}
           <Link
             href='/forget'
-            className='text-xs text-blue-600 hover:underline'
+            className='text-xs text-[#FEAF00] hover:underline'
           >
             Resset Password
           </Link>
